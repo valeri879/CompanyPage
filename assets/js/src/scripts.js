63c58a6298b8ec19e247cddbd356394c567f5298
valeri879 <valeri.kharitonashvili1@gmail.com>
2017-01-11 15:09:24 +0400

    res

--- assets/js/src/scripts.js
+++ assets/js/src/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
     var swiper = new Swiper('.swiper-container');
+
+    var swiper = new Swiper('.partners-slider', {
+        paginationClickable: true,
+        slidesPerView: 3,
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 0
+            },
+            420: {
+                slidesPerView: 1,
+                spaceBetween: 0
+            }
+        }
+    });
 });
